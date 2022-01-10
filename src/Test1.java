e23e076c382ea50186e927996a3d336dbfea08ea
Vishnu <vishnuvardhan.dondeti@gmail.com>
2022-01-10 16:22:01 +0530

    Two new files added

---
+++ src/Test1.java
@@ -0,0 +1,31 @@
+
+public class Swaptwonumbers {
+
+	public static void main(String[] args) {
+		
+		int a =10; int b= 20; int t = a;
+		
+//		a = b;
+//		
+//		b =t;
+//		http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=27025
+//		System.out.println("The value of a is " + a);
+//		System.out.println("The value of a is " + b);
+//		
+		
+		//second way 
+		
+		a = a+b;//30
+		b= a-b;//10
+		a= a-b;
+		System.out.println("The value of a is " + a);
+		System.out.println("The value of b is " + b);
+		
+				
+				
+				
+		
+
+	}
+
+}
